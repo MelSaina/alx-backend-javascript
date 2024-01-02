9177db8b346f9cfcdd31f9aaad37f26b124911e8
MelSaina <gabbysiana@gmail.com>
2024-01-02 20:22:42 +0300

    Task Requirements

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+export default function appendToEachArrayValue(array, appendString) {
+
+    const final = [];
+
+    for (const value of array) {
+      final.push(appendString + value);
+    }
+  
+    return final;
+}
