f292164a5918bc6df226533d98f95e27e3b33996
MelSaina <gabbysiana@gmail.com>
2024-01-08 14:09:55 +0000

    2

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,11 @@
+/*
+ * Lists students based on location
+ *
+ */
+
+export default function getStudentsByLocation(students, city){
+	if (students instanceof Array){
+		return students.filter((student) => student.location == city );
+	}
+	return [];
+}
