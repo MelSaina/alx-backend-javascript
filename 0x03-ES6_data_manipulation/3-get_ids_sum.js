63200d0f25ae4bc1ca46bc6d31383352a90e2d8c
MelSaina <gabbysiana@gmail.com>
2024-01-08 14:20:36 +0000

    3

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,10 @@
+/*
+ * Lists the sum of student Ids
+ */
+
+export default function getStudentIdsSum( students ){
+	if ( students instanceof Array ){
+		return students.reduce((sum, student) => sum + student.id, 0);
+	}
+	return [];
+}
